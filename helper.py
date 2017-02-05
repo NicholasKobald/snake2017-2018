22d130c4a0bf67a22f9a440b5998b6d4f3d6b9dc
Nicholas Kobald <nick.kobald@gmail.com>
2017-02-04 22:43:27 -0800

    fixes bugs with valid_moves

--- helper.py
+++ helper.py
@@ -26,23 +26,41 @@ def get_dir(coords):
     if coords[0][1] < coords[1][1]:
         return 'n'
     return 's'
+def print_board_two(board):
+    brs = ''
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board['state']=='empty':
+                brs+='E'
+            else:
+                brs+='S'
 
 def print_board(board):
     for row in board:
-        print row
+        row_rep  = []
+        for entry in row:
+            if entry['state']=='empty':
+                row_rep.append('e')
+            else:
+                row_rep.append('S')
+        print row_rep
+
 def get_valid_moves(snake, board, width=10, height=10):
     candidate_moves = ['n', 'e', 's', 'w']
-    candidate_moves.remove(flip_dict[get_dir(snake['coords'])])
+
     head = snake['coords'][0]
-    print_board(board)
-    
-    if head[0] == width-1:
+    not_safe = lambda y, x:board[x][y]['state']=='head' or board[x][y]['state']=='body'
+
+    y = head[0]
+    x = head[1]
+
+    if x == width-1 or not_safe(x+1, y):
         candidate_moves.remove('e')
-    if head[0] == 0:
+    if head[0] == 0 or not_safe(x-1, y):
         candidate_moves.remove('w')
-    if head[1] == height-1:
+    if head[1] == height-1 or not_safe(x, y+1):
         candidate_moves.remove('s')
-    if head[1] == 0:
+    if head[1] == 0 or not_safe(x, y-1):
         candidate_moves.remove('n')
 
     return candidate_moves
