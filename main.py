2b972df15954e9142318dcb2973f749d429d2d86
Nicholas Kobald <nick.kobald@gmail.com>
2017-02-13 13:11:47 -0800

    switches web framework to Flask.. bottle hot reload sucks

--- main.py
+++ main.py
@@ -3,36 +3,46 @@
 # N. Kobald - 2017-02-04
 
 import os, json
-from bottle import run, route, post, request
+from flask import Flask, request 
 from helper import * #helper functions
 from objects import * #helper classes
 from logic import * #meat of the algorithm
 
 OUR_SNAKE_NAME = '1'
 
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Hello World"
+
 def pick_move(game_obj):
     game_obj.print_board()
     print "Done pick_move"
+
 #page to dump data
-@route('/hello')
+@app.route('/hello')
 def hello():
     return "Hello World!"
 
-@route('/start', method='POST')
+@app.route('/start', methods=['POST'])
 def start():
-    print "In start"
-    data = json.loads(request.body.read()) #dict
+
+    data = request.data #dict
+
+    return data
+
     response = dict(
         color='#369',
         name='Bennet',
         taunt='My. Treat.'
     )
+
     return json.dumps(response)
 
-@route('/move', method='POST')
+@app.route('/move', methods=['POST'])
 def move():
     print "In Move. V2."
-    data = json.loads(request.body.read()) #dict
+    data = request.get_json(force=True) #dict
 
     height = len(data['board']) #only tested on N x N  (not N x M)
     width = len(data['board'][0])
@@ -40,18 +50,14 @@ def move():
     print "Playing on a", width, "by", height, "board."
 
     game = GameBoard(data['board'])
-    print "game created"
     pick_move(game)
 
     response = {
         'move':'up',
         'taunt':'Lets raise the ROOOF'
     }
+
     return json.dumps(response)
 
-
-def parse_input(board_info):
-    print board_info
-
-if __name__ == "__main__":
-    run(host='localhost', port=8080, debug=True, reloader=True)
+if __name__ == '__main__':
+    app.run(debug=True)
